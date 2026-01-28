55f9b247a684a69f5ef23399280a4224ff38818b
Apsara Aryal <100707766+APAryal@users.noreply.github.com>
2026-01-28 10:41:20 +0545

    this is numpy import and check the numpy version

---
+++ numpybasic/first.py
@@ -0,0 +1,12 @@
+# numpy is import
+import numpy 
+arr=numpy.array([1,3,5,6,7]) 
+print(arr)
+# numpy is usually imported under the np
+
+import numpy as np
+arr=numpy.array([1, 5, 6,7, 9])
+print(arr)
+# check the numpy version
+import numpy as np
+print(np.__version__)
