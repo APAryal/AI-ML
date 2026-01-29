2076225eb3870af01cd7e35d5ae1645f76f1e2f9
Apsara Aryal <100707766+APAryal@users.noreply.github.com>
2026-01-29 23:46:11 +0545

    this is step slicing and 2-D array sclicing

--- numpybasic/arrayslicing.py
+++ numpybasic/arrayslicing.py
@@ -14,3 +14,26 @@ print(arr[:4])
 import numpy as np
 arr=np.array([1,2,3,4,5,6,7,8])
 print(arr[-3:-1])
+# use the step value to determine the step of the slicing
+# return every other element from index 1 to index 5 differnt 2
+import numpy as np
+arr=np.array([1,2,3,4,5,6,7])
+print(arr[1:5:2])
+# return every other element from the entire array
+import numpy as np 
+arr=np.array([1,2,3,4,5,6,7])
+print(arr[::2])
+
+# from the second element slice elements from index 1 to index 4 (not included )
+import numpy as np 
+arr=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print(arr[1,1:4])
+
+# from the both element return index 2
+import numpy as np
+arr=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print(arr[0:2,2])
+# from both elements slice index 1 to index 4 (not included this will return a 2-d array)
+import numpy as np 
+arr=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print(arr[0:2, 1:4])
