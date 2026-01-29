f7e056bcde4d075d4790fee253f8b164795b8c64
Apsara Aryal <100707766+APAryal@users.noreply.github.com>
2026-01-29 23:21:17 +0545

    this is array slice

---
+++ numpybasic/arrayindexing.py
@@ -0,0 +1,22 @@
+# get the first element from the following array
+import numpy as np
+arr=np.array([1,5,7,8])
+print(arr[0])
+# get the second element from the following array
+import numpy as np
+arr=np.array([4,6,7,8])
+print(arr[1])
+
+# get third and fourth element from the following array and add them
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print(arr[2]+arr[3])
+
+# access the element on the first row and second column
+import numpy as np
+arr=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print('2nd element on 1st row:',arr[0,1])
+# Access the element on the second row and fifth column
+import numpy as no
+arr=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print("second row 5th element",arr[1,4])
