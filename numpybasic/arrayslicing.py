2d088a18da18bbaf954ba35e619220d3341cb5b5
Apsara Aryal <100707766+APAryal@users.noreply.github.com>
2026-01-29 23:29:48 +0545

    this is negative numpy array sliceing

--- numpybasic/arrayslicing.py
+++ numpybasic/arrayslicing.py
@@ -2,3 +2,15 @@
 import numpy as np
 arr=np.array([1,2,3,4,5,6])
 print(arr[1:5])
+# slice elements from index 4 to the end of the array
+import numpy as np
+arr=np.array([1,2,3,4,5,6,7])
+print(arr[4:])
+# slice element from the beginning to index 4  not include
+import numpy as np
+arr=np.array([1,2,3,4,5,6,7,8])
+print(arr[:4])
+# slice form the index 3 form the end index 1 for the end usi the minus operator to refer to an index from the end
+import numpy as np
+arr=np.array([1,2,3,4,5,6,7,8])
+print(arr[-3:-1])
