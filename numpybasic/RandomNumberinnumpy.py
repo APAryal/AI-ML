39399ab5e1a0d252e6efa521bd15b922fb3464da
Apsara Aryal <100707766+APAryal@users.noreply.github.com>
2026-02-08 00:16:14 +0545

    this is random float and interger number generate

--- numpybasic/RandomNumberinnumpy.py
+++ numpybasic/RandomNumberinnumpy.py
@@ -11,4 +11,23 @@ print(x)
 from numpy import random
 x=random.randint(100, size=(5))
 print(x)
-# 
+# Generate a 2-D array with 3 rows each rew containing 5 random integers from 1 to 100
+from numpy import random
+x=random.randint(100, size=(3,5))
+print(x)
+# Generate a 1-D array containing 5 random floats
+from numpy import random
+x=random.rand(5)
+print(x)
+# Generate a 2_d array with 3 rows each row containing 5 random numbers
+from numpy import random
+x= random.rand(3,5)
+print(x)
+# Return one of the values in array:
+from numpy import random
+x=random.choice([3,5,7,9])
+print(x)
+# generate a 2-D array that consists of the valuse in the array parameter (3,5,7, and 9):
+from numpy import random
+x=random.choice([3,5,7,9],size=(3,5))
+print(x)
