8f17311e17d301438dc149271300bb0c83bb9e06
Apsara Aryal <100707766+APAryal@users.noreply.github.com>
2026-02-08 13:23:41 +0545

    this is Random permutation

---
+++ numpybasic/RandomPermutations.py
@@ -0,0 +1,12 @@
+# Random shuffle elements of following array
+from numpy import random
+import numpy as np
+arr=np.array([1,2,3,4,5])
+random.shuffle(arr)
+print(arr)
+# Generate a random permutation of elements of following array 
+from numpy import random
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print(random.permutation(arr))
+print(arr)
