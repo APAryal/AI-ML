4672fe232159091eab151b00125b40d16c3c9ef8
Apsara Aryal <100707766+APAryal@users.noreply.github.com>
2026-02-08 14:25:04 +0545

    seaborn practice

---
+++ numpybasic/seaborn/Seaborn.py
@@ -0,0 +1,11 @@
+# plotting a Displot
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.displot([0,1,2,3,4,5])
+plt.show()
+
+# plotting a displot without the Histogram
+import matplotlib .pyplot as plt
+import seaborn as sns
+sns.displot([0,1,2,3,4,5], kind="kde")
+plt.show()
