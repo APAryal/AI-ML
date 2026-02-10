8a9ad58cd73a2270ca8c24b8f2183351b793ec3c
Apsara Aryal <100707766+APAryal@users.noreply.github.com>
2026-02-10 11:20:20 +0545

    this is the import pandas and check the pandas version

--- pandas/first.py
+++ pandas/first.py
@@ -8,4 +8,18 @@ print(df['city'])
 print(df['name'])
 print(df[df['age']<25])
 print(df.describe())
+# import pandas
+import pandas
+# now pandas is imported and ready to use
+import pandas
+mydataset={
+    'cars':["BMW", "Volvo", "ford"],
+    "passinfs":[3,7,2]
+}
+myvar=pandas.DataFrame(mydataset)
+print(myvar)
 
+# checking the pandas version 
+# the version string is stored under _version_ attribute.
+import pandas as pd
+print(pd.__version__)
