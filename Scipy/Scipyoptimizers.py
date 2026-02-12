4e48a086243900029cb9d192b412bca4bbc02ea7
Apsara Aryal <100707766+APAryal@users.noreply.github.com>
2026-02-13 01:29:11 +0545

    scipyoptimize

--- Scipy/Scipyoptimizers.py
+++ Scipy/Scipyoptimizers.py
@@ -1,3 +1,23 @@
-import scipy
 
-print(scipy.__version__)
+
+# Find root of the equation x + cos(x):
+from scipy.optimize import root
+from numpy import cos
+def eqn(x):
+    return x+cos(x)
+myroot=root(eqn,0)
+print(myroot.x)
+# Print all information about the solution (not just x which is the root)
+
+from scipy.optimize import root
+from numpy import cos
+def eqn(x):
+    return x+cos(x)
+myroot=root(eqn, 0)
+print(myroot)
+# minimize the function x^2 +x+2 with BFGS:
+from scipy.optimize import minimize
+def eqn(x):
+    return x**2 +x+2
+mymin=minimize(eqn, 0, method='BFGS')
+print(mymin)
